8e9a468c49c3de9db26955dfb963e48465215c43
manoj7yadav <my941199@gmail.com>
2025-12-25 17:38:56 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -18,8 +18,7 @@ export default function LandingPage() {
         
         {/* Logo */}
         <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-          <div onmouseover="this.style.transform='scale(1.2)'"
-              onmouseout="this.style.transform='scale(1)'" style={{ width: '32px', height: '32px', background: '#4285F4', borderRadius: '8px', display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'white', fontWeight: 'bold' }}>J</div>
+          <div style={{ tranform:scale(1.1), width: '32px', height: '32px', background: '#4285F4', borderRadius: '8px', display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'white', fontWeight: 'bold' }}>J</div>
           <a id="joblinklogo" rel="" href="">
           <span style={{ fontSize: '1.25rem', fontWeight: 'bold', color: '#333' }}>JobLink</span>
           </a>
