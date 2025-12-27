455f4e6ad6c5330c3b068eb5e1f0ba94accf2b9b
mayankyadav00 <79706302+mayankyadav00@users.noreply.github.com>
2025-12-27 17:04:56 +0530

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -11,7 +11,7 @@ export default function LoginPage() {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window}/seeker/dashboard`, // Where to go after login
+          redirectTo: `${window.location.origin}/seeker/dashboard`, // Where to go after login
         },
       });
 
@@ -46,5 +46,6 @@ export default function LoginPage() {
     </div>
   );
 }
+
 
 
