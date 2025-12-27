1a489e79b772de86a1462ecdd2c034a619d8ef8b
mayankyadav00 <79706302+mayankyadav00@users.noreply.github.com>
2025-12-27 17:17:52 +0530

    Update superbase.js

--- src/lib/superbase.js
+++ src/lib/superbase.js
@@ -1,1 +1,14 @@
+// src/lib/supabase.js
+import { createClient } from '@supabase/supabase-js';
 
+// 1. Read the secret variables
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// 2. Error check (Optional but helpful for debugging)
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing Supabase Environment Variables!");
+}
+
+// 3. Connect
+export const supabase = createClient(supabaseUrl, supabaseKey);
