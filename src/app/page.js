17e0fc0768694edbce984a47517a7ede5263e6ac
manoj7yadav <my941199@gmail.com>
2026-01-04 22:07:17 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -60,7 +60,7 @@ export default function LandingPage() {
     overflow: 'hidden' // Ensures logo stays inside the box
   }}>
     <img 
-      src="/src/logo.png" 
+      src="file:///C:/Users/LENOVO/OneDrive/Desktop/LOGO.png" 
       alt="JobLink Logo" 
       style={{ width: '100%', height: '100%', objectFit: 'contain' }} 
     />
